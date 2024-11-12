e4f0f436e3dafb0c6cc423fe8af3be67d2037e77
mrKrenar <piraj.krenar@gmail.com>
2024-11-12 02:15:48 +0100

    mini changes

--- Assets/_Project/Scripts/SetAnimatorBoolean.cs
+++ Assets/_Project/Scripts/SetAnimatorBoolean.cs
@@ -5,15 +5,9 @@ public class SetAnimatorBoolean : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private string boolKeyToSet;
-    private static int keyHash;
-
-    private void Awake()
-    {
-        keyHash = Animator.StringToHash(boolKeyToSet);
-    }
 
     public void SetValue(bool value)
     {
-        animator.SetBool(keyHash, value);
+        animator.SetBool(boolKeyToSet, value);
     }
 }
